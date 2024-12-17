ed8814bdbacb842d3a46bcebfc1d3e581a3b60cd
mostafar <mostafar@backbase.com>
2024-12-17 16:57:08 +0100

    Fixing Review comments

--- token-management/src/main/java/com/backbase/goldensample/token/Application.java
+++ token-management/src/main/java/com/backbase/goldensample/token/Application.java
@@ -1,15 +1,12 @@
 package com.backbase.goldensample.token;
 
-import com.backbase.audit.client.EnableAuditClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableAuditClient
 @EnableDiscoveryClient
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
--- token-provider-outbound-service/src/main/java/com/backbase/goldensample/token/outbound/Application.java
+++ token-provider-outbound-service/src/main/java/com/backbase/goldensample/token/outbound/Application.java
@@ -2,12 +2,9 @@ package com.backbase.goldensample.token.outbound;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
